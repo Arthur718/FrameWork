e546a2b2ee7da55e8ae932e97123fe82c4cc43d4
Arthur Avros <>
2019-04-14 18:26:23 -0400

    new class Object Reposetory

---
+++ src/main/java/OR/PageObjectBase.java
@@ -0,0 +1,11 @@
+package OR;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageObjectBase {
+    protected WebDriver driver= null;
+    public  PageObjectBase(WebDriver driver){
+        this.driver=driver;
+
+    }
+}
